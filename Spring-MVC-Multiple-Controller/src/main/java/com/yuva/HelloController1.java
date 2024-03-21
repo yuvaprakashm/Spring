8d79ebaf91f5d04ca1a5e4e6389a1d4882450fe6
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-03-21 17:23:55 +0530

    Add files via upload

---
+++ Spring-MVC-Multiple-Controller/src/main/java/com/yuva/HelloController1.java
@@ -0,0 +1,12 @@
+package com.yuva;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController1 {
+	@RequestMapping("/hello1")
+	public String display() {
+		return "viewpage1";
+	}
+}
---
+++ Spring-MVC-Multiple-view-Page/src/main/webapp/WEB-INF/jsp/viewpage.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<a href="helloagain">java Tutorials</a>
+</body>
+</html>
